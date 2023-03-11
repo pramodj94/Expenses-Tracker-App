1d3e7efe204cf14780639bbbffd5e9f24165489e
pramod jadhav <pramodnaygaon@gmail.com>
2023-03-11 10:12:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Expenses from "./componants/Expenses";
+import NewExpense from "./componants/NewExpenses/NewExpense";
 
-function App() {
+let DUMMY_EXPENSE  =[]
+    
+  const App=()=>{
+    const[expenses,setExpenses]=useState(DUMMY_EXPENSE)
+    useEffect(()=>{ fetch("https://techgun.website/sample/api/read.php" ).then(response=>{
+      return response.json()
+    }).then (data =>{setExpenses(data)})},[])
+  
+   
+  const addExpenseHandler =(expense)=>{
+    const udatedExpense =[expense ,...expenses]
+    setExpenses(udatedExpense )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <NewExpense onAddExpense ={addExpenseHandler}/>
+     <Expenses item ={expenses}/>
+    </>
+  )
 }
 
-export default App;
+
+export default App
+
